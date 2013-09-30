30815ec7b86ca87e328895b9c915d41e12436b39
keisuke <keisuke.uchimura@gengo.com>
2013-09-30 11:27:47 +0900

    this is the sample selenium with nose.

--- sample_selenium_test.py
+++ sample_selenium_test.py
@@ -4,25 +4,30 @@
 from selenium import webdriver
 
 
-if __name__ == "__main__":
-    fp = webdriver.FirefoxProfile()
-    fp.set_preference("intl.accept_languages", "en-us,en,ja")
-    fp.update_preferences()
+class TestSampleSelenium(object):
 
-    driver = webdriver.Remote(
-        command_executor='http://127.0.0.1:4444/wd/hub',
-        desired_capabilities={
-            'platform': 'ANY',
-            'browserName': 'firefox',
-            'version': '',
-            'javascriptEnabled': True
-        },
-        browser_profile=fp
-    )
+    def setup(self):
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", "en-us,en,ja")
+        fp.update_preferences()
 
-    driver.get("http://gengo.com")
-    elem = driver.find_element_by_id("translation")
-    print driver.current_url
-    print driver.title
-    print elem.text
-    driver.quit()
+        self.driver = webdriver.Remote(
+            command_executor='http://127.0.0.1:4444/wd/hub',
+            desired_capabilities={
+                'platform': 'ANY',
+                'browserName': 'firefox',
+                'version': '',
+                'javascriptEnabled': True
+            },
+            browser_profile=fp
+        )
+
+    def test_open_gengo(self):
+        self.driver.get("http://gengo.com")
+        elem = self.driver.find_element_by_id("translation")
+        print self.driver.current_url
+        print self.driver.title
+        print elem.text
+
+    def teardown(self):
+        self.driver.quit()
